ceb5aed1afb7118d239e9e72cb58e3d052565c62
Dmitry Zinkevich <dmitry.zinkevich@boku.com>
2024-05-30 13:48:18 +0300

    Create vectors

--- ray-tracer/src/main/java/foo/bar/geom/Geometry.java
+++ ray-tracer/src/main/java/foo/bar/geom/Geometry.java
@@ -7,4 +7,8 @@ public class Geometry {
     public static double[] newPoint(double x, double y, double z) {
         return new double[] {x, y, z, 1.0};
     }
+
+    public static double[] newVector(double x, double y, double z) {
+        return new double[] {x, y, z, 0.0};
+    }
 }
--- ray-tracer/src/test/java/foo/bar/geom/GeometryTest.java
+++ ray-tracer/src/test/java/foo/bar/geom/GeometryTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 public class GeometryTest {
     @Test
     public void should_create_point() {
-        // given
-
         // when
         var point = Geometry.newPoint(4.3, -4.2, 3.1);
 
@@ -18,6 +16,20 @@ public class GeometryTest {
             it.assertThat(point[1]).isEqualTo(-4.2);
             it.assertThat(point[2]).isEqualTo(3.1);
             it.assertThat(point[3]).isEqualTo(1.0);
+        });
+    }
+
+    @Test
+    public void should_create_vector() {
+        // when
+        var vector = Geometry.newVector(4.3, -4.2, 3.1);
+
+        // then
+        SoftAssertions.assertSoftly(it -> {
+            it.assertThat(vector[0]).isEqualTo(4.3);
+            it.assertThat(vector[1]).isEqualTo(-4.2);
+            it.assertThat(vector[2]).isEqualTo(3.1);
+            it.assertThat(vector[3]).isEqualTo(0.0);
         });
     }
 }
