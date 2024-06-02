fd1644080021b54259e5faec976b3ed3380c173d
Dmitry Zinkevich <dmitry.zinkevich@boku.com>
2024-06-02 21:49:07 +0300

    Create 2x2 and 3x3 matrices

--- src/main/java/foo/bar/math/Matrix.java
+++ src/main/java/foo/bar/math/Matrix.java
@@ -3,21 +3,26 @@ package foo.bar.math;
 import java.util.Objects;
 
 public class Matrix {
-    private final static int DIMENSION = 4;
     private final double[] values;
+    private final int dimension;
 
     public Matrix(double[] values) {
-        if (Objects.requireNonNull(values).length != 16) {
-            throw new IllegalArgumentException("Matrix should have 16 elements");
+        Objects.requireNonNull(values);
+        if (values.length != 4 && values.length != 9 && values.length != 16) {
+            throw new IllegalArgumentException("Matrix can have 4, 9 or 16 elements");
         }
         this.values = values;
+        this.dimension = (int) Math.sqrt(values.length);
     }
 
     public double at(int row, int col) {
+        if (row >= dimension || col >= dimension) {
+            throw new IllegalArgumentException("Row or column out of bounds");
+        }
         return values[idx(row, col)];
     }
 
     private int idx(int row, int col) {
-        return row * DIMENSION + col;
+        return row * dimension + col;
     }
 }
--- src/test/java/foo/bar/math/MatrixTest.java
+++ src/test/java/foo/bar/math/MatrixTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 public class MatrixTest {
     @Test
-    public void should_create_new_matrix() {
+    public void should_create_4x4_matrix() {
         // given
         var matrix = new Matrix(
             new double[] {
-                    1, 2, 3, 4,
-                    5.5, 6.5, 7.5, 8.5,
-                    9, 10, 11, 12,
-                    13.5, 14.5, 15.5, 16.5
+                1, 2, 3, 4,
+                5.5, 6.5, 7.5, 8.5,
+                9, 10, 11, 12,
+                13.5, 14.5, 15.5, 16.5
             }
         );
 
@@ -24,6 +24,46 @@ public class MatrixTest {
             it.assertThat(matrix.at(2, 2)).isEqualTo(11);
             it.assertThat(matrix.at(3, 0)).isEqualTo(13.5);
             it.assertThat(matrix.at(3, 2)).isEqualTo(15.5);
+        });
+    }
+
+    @Test
+    public void should_create_2x2_matrix() {
+        // given
+        var matrix = new Matrix(
+            new double[] {
+               -3, 5,
+                1, -2
+            }
+        );
+
+        // then
+        SoftAssertions.assertSoftly(it -> {
+            it.assertThat(matrix.at(0, 0)).isEqualTo(-3);
+            it.assertThat(matrix.at(0, 1)).isEqualTo(5);
+            it.assertThat(matrix.at(1, 0)).isEqualTo(1);
+            it.assertThat(matrix.at(1, 1)).isEqualTo(-2);
+        });
+    }
+
+    @Test
+    public void should_create_3x3_matrix() {
+        // given
+        var matrix = new Matrix(
+            new double[] {
+                -3, 5, 0,
+                1, -2, -7,
+                0, 0, 1
+            }
+        );
+
+        // then
+        SoftAssertions.assertSoftly(it -> {
+            it.assertThat(matrix.at(0, 0)).isEqualTo(-3);
+            it.assertThat(matrix.at(0, 1)).isEqualTo(5);
+            it.assertThat(matrix.at(1, 0)).isEqualTo(1);
+            it.assertThat(matrix.at(1, 1)).isEqualTo(-2);
+            it.assertThat(matrix.at(2, 2)).isEqualTo(1);
         });
     }
 }
